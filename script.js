d1c1f59c2e86e23c7838bb09e7d14a12791e1fb6
Roman <aktualnoe.net@yandex.ru>
2019-07-02 21:34:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/*var number = 5;
+var string = "hello";
+var sum = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string' * 9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone['name']);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[2]);*/
+
+//alert('Hello world');
+
+/*let answer = confirm('Are you here?');
+console.log(answer);*/
+
+/*let answer = prompt('Are you here?', 'Yes');
+console.log(answer);*/
+
+/*let answer = confirm('Are you here?');
+console.log(answer);*/
+
+//let answer = +prompt('Are you here?', 'yes');
+
+//console.log(typeof(answer));
+
+var incr = 10;
+var decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+console.log('2' === 2);
+
+let isChecked = true;
+let isClose = false;
+
+console.log(isChecked || !isClose);
