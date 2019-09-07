f67d5aeebbdeb48b64ea92f16a257d5d36aa88f8
Roman <aktualnoe.net@yandex.ru>
2019-09-07 16:59:58 +0300

    Начало мазафака

--- script.js
+++ script.js
@@ -1,55 +1,11 @@
 'use strict';
 
-/*var number = 5;
-var string = "hello";
-var sum = Symbol();
-var boolean = true;
-null;
-undefined;
-var obj = {};
-
-console.log(4/0);
-console.log('string' * 9);
-
-let something;
-console.log(something);
-
-let persone = {
-    name: 'John',
-    age: 25,
-    isMarried: false
-}
-
-console.log(persone['name']);
-
 let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-console.log(arr[2]);*/
 
-//alert('Hello world');
+console.log(arr[2]);
 
-/*let answer = confirm('Are you here?');
-console.log(answer);*/
+alert('hello!');
 
-/*let answer = prompt('Are you here?', 'Yes');
-console.log(answer);*/
+let answer = prompt('Are you here?', 'yes')
 
-/*let answer = confirm('Are you here?');
-console.log(answer);*/
-
-//let answer = +prompt('Are you here?', 'yes');
-
-//console.log(typeof(answer));
-
-var incr = 10;
-var decr = 10;
-
-console.log(incr++);
-console.log(decr--);
-
-console.log(5 % 2);
-console.log('2' === 2);
-
-let isChecked = true;
-let isClose = false;
-
-console.log(isChecked || !isClose);
+console.log(typeof(answer));
